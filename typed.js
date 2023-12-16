8a388f46ab988d3cf5a097acaca4cb235440dd2d
William Air <william.paciaroni97@gmail.com>
2023-12-16 15:32:29 +0100

    Added particle.js

---
+++ typed.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['C#', 'Angular', 'SQL Server', 'Entity Framework', '.NET'],
+    typeSpeed: 50,
+    backSpeed: 60,
+    loop: true,
+    backDelay: 700
+  });
